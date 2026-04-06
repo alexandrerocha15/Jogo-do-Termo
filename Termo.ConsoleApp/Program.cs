6ff2067c3e8ee0f06b8628bb964bcb0e43505190
Alexandre Rocha <juny14_15@hotmail.com>
2026-04-05 21:25:26 -0300

    Adicionado mais palavrtas

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -214,8 +214,8 @@ class Program
 
 static class BancoPalavras
 {
-    public static string[] palavras = {"vento", "terra", "livro"}; /*, "canil", "leite", "carro", 
+    public static string[] palavras = {"vento", "terra", "livro", "canil", "leite", "carro", 
         "porta", "chave", "peixe", "cobra", "porco", "cinto", "calca", "praia", "areia", "nuvem",
         "chuva", "vento", "calor", "verde", "preto", "amigo", "irmao", "filho", "arroz", "caixa"
-    };*/
+    };
 }
